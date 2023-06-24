5fb21a910bc8fbbc7a81f3623d2c729f15dba116
kokitree <94977757+tharumbun@users.noreply.github.com>
2023-06-24 23:16:03 +0700

    Update constants.ts

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -12,7 +12,7 @@ export const MENU_LINKS = [
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
+	twitter: "https://twitter.com/cambopedia",
 	discord: "https://github.com/chrismwilliams/astro-cactus",
 	email: "me@example.com",
 };
